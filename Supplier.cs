0a2911932aed7bed8ee2af4064bf09b02606bf0b
GamalCode <66071550+GamalCode@users.noreply.github.com>
2025-11-06 09:21:03 +0100

    Remove Contact property from Supplier class
    Removed 'Contact' property from Supplier class.

--- Supplier.cs
+++ Supplier.cs
@@ -1,10 +1,9 @@
-﻿namespace Domain.Models
+namespace Domain.Models
 {
     public class Supplier
     {
         public int Supplier_ID { get; set; }
         public string Name { get; set; }
-        public string Contact { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
     }
